14d872f3b1e82392b2cbbdb84433794f27585fc4
Landon Coddington <lcoddington1@ewu.edu>
2026-01-11 21:14:06 +0000

    Add initial HTML structure, CSS styles, and JavaScript functionality
    - Created index.html with a basic structure and content about Mozilla.
    - Added main.js to change the heading text to "Hello world!".
    - Introduced style.css for styling the page, including font settings, colors, and layout.

---
+++ week-0/behavioral-layer/scripts/main.js
@@ -0,0 +1,4 @@
+// puts the h1 in a 'container' so you can manipulate it
+const myHeading = document.querySelector("h1");
+//changes what the heading says so now it says "Hello world!"
+myHeading.textContent = "Hello world!";
