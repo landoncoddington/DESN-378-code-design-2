9ce1785163a167f45bc3493fe2ed2d967b4d2478
Landon Coddington <lcoddington1@ewu.edu>
2026-02-02 17:30:02 -0800

    Update script.js
    Signed-off-by: Landon Coddington <lcoddington1@ewu.edu>

--- week-1/portfolio/scripts/script.js
+++ week-1/portfolio/scripts/script.js
@@ -5,27 +5,26 @@ const savedTheme = localStorage.getItem('theme');
 
 if (savedTheme) {
 
-  if (savedTheme === 'dark') {
-    document.body.classList.add('dark');
+document.documentElement.dataset.theme= savedTheme;
   }
-} else {
 
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-  if (prefersDark.matches) {
-    document.body.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
-  }
-}
+const toggle = dpcument.querySelector ('theme-toggle'); 
 
+toggle.addEventListener ('click', function () {
+  const currentTheme = document.documentElement.dataset.theme;
 
-toggle.addEventListener('click', function() {
-  const isDark = document.body.classList.contains('dark');
-
-  if (isDark) {
-    document.body.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
+  let newTheme; 
+  if (currentTheme === 'dark') {
+    newTheme = 'light' ;
   } else {
-    document.body.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
+    newTheme = 'dark' ;
   }
+
+  document.documentElement.dataset.theme = newTheme;
+  localStorage.setItem('theme', newTheme);
 });
+
+
+
+
+
